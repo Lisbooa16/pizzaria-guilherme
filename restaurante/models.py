b9f3b646490c08cc82aa1d2698ae66f9165db433
Guilherme Lisboa <guibrbr161@gmail.com>
2022-09-29 14:19:26 -0300

    criando um modelo de prato

---
+++ restaurante/models.py
@@ -0,0 +1,11 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class prato(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(decimal_places=2, max_digits=18)
+    imagem = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.nome
