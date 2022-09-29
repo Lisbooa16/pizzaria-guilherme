b0a7e26159fa94d44f7c96e033bf6bad6f997267
Guilherme Lisboa <guibrbr161@gmail.com>
2022-09-29 15:05:47 -0300

    ajuste nessa bomba5

--- restaurante/models.py
+++ restaurante/models.py
@@ -1,5 +1,5 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
 
 class prato(models.Model):
     nome = models.CharField(max_length=255)
